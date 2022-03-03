af2550f0c6f4c03aa2bae03e00468254e2fe9d47
cristhian <cristhianj.armijosc@gmail.com>
2022-03-03 12:12:07 -0500

    cambios

--- resources/views/admin/formatosPDF/chequeImpresionPdf.blade.php
+++ resources/views/admin/formatosPDF/chequeImpresionPdf.blade.php
@@ -8,12 +8,12 @@
 </style>
 <body>
 
-    <div id="id1" style="overflow:hidden !important; font-size: 13px; position:absolute;left:{{$chequeImpresion->chequei_beneficiariox}}px;top: {{$chequeImpresion->chequei_beneficiarioy}}px;cursor:pointer;width:340px;height:18px;">@if(isset($cheque->cheque_beneficiario)){{$cheque->cheque_beneficiario}} @else Juan Piwabe Ortiz @endif</div>
+    <div id="id1" style="overflow:hidden !important; font-weight: bold; font-size: 13px; position:absolute;left:{{$chequeImpresion->chequei_beneficiariox}}px;top: {{$chequeImpresion->chequei_beneficiarioy}}px;cursor:pointer;width:340px;height:18px;">@if(isset($cheque->cheque_beneficiario)){{$cheque->cheque_beneficiario}} @else JUAN PIWABE ORTIZ @endif</div>
 
-    <div id="id2" style="position:absolute;left:{{$chequeImpresion->chequei_valorx}}px;top: {{$chequeImpresion->chequei_valory}}px;cursor:pointer;width:100px;height:18px;">@if(isset($cheque->cheque_beneficiario)){{number_format($cheque->cheque_valor,2,'.',',')}} @else 15805.89 @endif</div>
+    <div id="id2" style="position:absolute; font-weight: bold;left:{{$chequeImpresion->chequei_valorx}}px;top: {{$chequeImpresion->chequei_valory}}px;cursor:pointer;width:100px;height:18px;">@if(isset($cheque->cheque_beneficiario)){{number_format($cheque->cheque_valor,2,'.',',')}} @else 15805.89 @endif</div>
 
-    <div id="id3"  style="font-size: 13px; position:absolute;left:{{$chequeImpresion->chequei_fechax}}px;top: {{$chequeImpresion->chequei_fechay}}px;cursor:pointer;width:200px;height:18px;">@if(isset($cheque->cheque_beneficiario)){{$cheque->empresa->empresa_ciudad.', '.DateTime::createFromFormat('Y-m-d', $cheque->cheque_fecha_pago)->format('Y/m/d')}} @else 27/12/2021 @endif</div>
+    <div id="id3"  style="font-size: 13px; font-weight: bold;position:absolute;left:{{$chequeImpresion->chequei_fechax}}px;top: {{$chequeImpresion->chequei_fechay}}px;cursor:pointer;width:200px;height:18px;">@if(isset($cheque->cheque_beneficiario)){{$cheque->empresa->empresa_ciudad.', '.DateTime::createFromFormat('Y-m-d', $cheque->cheque_fecha_pago)->format('Y/m/d')}} @else MACHALA, 27/12/2021 @endif</div>
 
-    <div id="id4" style=" font-size: 13px;position:absolute;left:{{$chequeImpresion->chequei_letrasx}}px;top: {{$chequeImpresion->chequei_letrasy}}px;cursor:pointer;width:450px;height:36px;">@if(isset($cheque->cheque_beneficiario)) {{str_replace('DOLARES',' ',$cheque->cheque_valor_letras)}} @else Quince mil ochocientos cinco con 89/100 dolares @endif</div>
+    <div id="id4" style=" font-size: 13px; font-weight: bold;position:absolute;left:{{$chequeImpresion->chequei_letrasx}}px;top: {{$chequeImpresion->chequei_letrasy}}px;cursor:pointer;width:450px;height:36px;">@if(isset($cheque->cheque_beneficiario)) {{str_replace('DOLARES',' ',$cheque->cheque_valor_letras)}} @else QUINCE MIL OCHOCIENTOS CINCO CON 89/100 @endif</div>
 
 </body>

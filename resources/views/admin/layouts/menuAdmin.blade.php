801af70bd638adce9403e2cef4c41dfa8f689479
Arcam622 <jorge-6-19@hotmail.com>
2022-03-10 15:55:26 -0500

    cambios

--- resources/views/admin/layouts/menuAdmin.blade.php
+++ resources/views/admin/layouts/menuAdmin.blade.php
@@ -355,7 +355,7 @@
                                             </a>
                                         </li>
                                         @endif
-                                        @if(str_replace('D/E', '', $permiso->permiso_ruta) == 'decimoC' and $rango->tipoComprobante->tipo_comprobante_codigo == 'DS22')
+                                        @if(str_replace('D/E', '', $permiso->permiso_ruta) == 'decimoC' and $rango->tipoComprobante->tipo_comprobante_codigo == 'DS23')
                                         <li class="nav-item">
                                             <!--<a href="{{ url("{$ruta}/new/{$p->punto_id}") }}" class="nav-link">-->
                                             <a href="{{str_replace('D/E', '', $permiso->permiso_ruta)}}/new/{{ $p->punto_id}}" class="nav-link">

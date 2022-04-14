65f911e3d1eeea033622ea4b4dda117ea3d04932
Arcam622 <jorge-6-19@hotmail.com>
2022-04-14 16:34:55 -0500

    cambios

--- resources/views/admin/layouts/menuAdmin.blade.php
+++ resources/views/admin/layouts/menuAdmin.blade.php
@@ -382,6 +382,15 @@
                                             </a>
                                         </li>
                                         @endif
+                                        @if(str_replace('D/E', '', $permiso->permiso_ruta) == 'beneficioSocial' and $rango->tipoComprobante->tipo_comprobante_codigo == 'DS24')
+                                        <li class="nav-item">
+                                            <!--<a href="{{ url("{$ruta}/new/{$p->punto_id}") }}" class="nav-link">-->
+                                            <a href="{{str_replace('D/E', '', $permiso->permiso_ruta)}}/new/{{ $p->punto_id}}" class="nav-link">
+                                            <i class="far fa-dot-circle nav-icon"></i>
+                                            <p>{{ $rango->rango_descripcion.' - '.$p->sucursal->sucursal_codigo.$p->punto_serie }}</p>
+                                            </a>
+                                        </li>
+                                        @endif
                                         @endforeach
                                     @endif
                                 @endif

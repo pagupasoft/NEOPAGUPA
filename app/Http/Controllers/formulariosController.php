ca4011942b4eb078a173c148181dc5d41d68024b
cristhian <cristhianj.armijosc@gmail.com>
2022-06-10 08:29:22 -0500

    reporte tributario

--- app/Http/Controllers/formulariosController.php
+++ app/Http/Controllers/formulariosController.php
@@ -696,7 +696,8 @@ class formulariosController extends Controller
                         $compra = Transaccion_Compra::TransaccionSinFecha()
                         ->join('sustento_tributario','sustento_tributario.sustento_id','=','transaccion_compra.sustento_id')
                         ->join('detalle_tc','detalle_tc.transaccion_id','=','transaccion_compra.transaccion_id')->select('sustento_tributario.sustento_venta12')
-                        ->distinct('sustento_tributario.sustento_venta12')->where('detalle_tc.producto_id','=',$detalle->producto_id)->first();
+                        ->distinct('sustento_tributario.sustento_venta12')->where('detalle_tc.producto_id','=',$detalle->producto_id)
+                        ->where('tipo_comprobante.tipo_comprobante_codigo','=','01')->first();
                         if(isset($compra->sustento_venta12)){
                             if($compra->sustento_venta12 == '401' or $detalle->producto->producto_compra_venta == '2'){
                                 $datos[1]['sustento'] = 'Ventas locales (excluye activos fijos) gravadas tarifa diferente de cero';
@@ -726,7 +727,8 @@ class formulariosController extends Controller
                         $compra = Transaccion_Compra::TransaccionSinFecha()
                         ->join('sustento_tributario','sustento_tributario.sustento_id','=','transaccion_compra.sustento_id')
                         ->join('detalle_tc','detalle_tc.transaccion_id','=','transaccion_compra.transaccion_id')->select('sustento_tributario.sustento_venta12')
-                        ->distinct('sustento_tributario.sustento_venta12')->where('detalle_tc.producto_id','=',$detallenc->producto_id)->first();
+                        ->distinct('sustento_tributario.sustento_venta12')->where('detalle_tc.producto_id','=',$detallenc->producto_id)
+                        ->where('tipo_comprobante.tipo_comprobante_codigo','=','01')->first();
                         if(isset($compra->sustento_venta12)){
                             if($compra->sustento_venta12 == '401' or $detalle->producto->producto_compra_venta == '2'){
                                 $datos[1]['nc'] = floatval($datos[1]['nc']) + $detallenc->detalle_total; 
@@ -790,7 +792,8 @@ class formulariosController extends Controller
                         $compra = Transaccion_Compra::TransaccionSinFecha()
                         ->join('sustento_tributario','sustento_tributario.sustento_id','=','transaccion_compra.sustento_id')
                         ->join('detalle_tc','detalle_tc.transaccion_id','=','transaccion_compra.transaccion_id')->select('sustento_tributario.sustento_venta0')
-                        ->distinct('sustento_tributario.sustento_venta0')->where('detalle_tc.producto_id','=',$detalle->producto_id)->first();
+                        ->distinct('sustento_tributario.sustento_venta0')->where('detalle_tc.producto_id','=',$detalle->producto_id)
+                        ->where('tipo_comprobante.tipo_comprobante_codigo','=','01')->first();
                         if(isset($compra->sustento_venta0)){
                             if($compra->sustento_venta0 == '403'){
                                 $datos[1]['sustento'] = 'Ventas locales (excluye activos fijos) gravadas tarifa 0% que no dan derecho a crédito tributario';
@@ -834,7 +837,8 @@ class formulariosController extends Controller
                         $compra = Transaccion_Compra::TransaccionSinFecha()
                         ->join('sustento_tributario','sustento_tributario.sustento_id','=','transaccion_compra.sustento_id')
                         ->join('detalle_tc','detalle_tc.transaccion_id','=','transaccion_compra.transaccion_id')->select('sustento_tributario.sustento_venta0')
-                        ->distinct('sustento_tributario.sustento_venta0')->where('detalle_tc.producto_id','=',$detallenc->producto_id)->first();
+                        ->distinct('sustento_tributario.sustento_venta0')->where('detalle_tc.producto_id','=',$detallenc->producto_id)
+                        ->where('tipo_comprobante.tipo_comprobante_codigo','=','01')->first();
                         if(isset($compra->sustento_venta0)){
                             if($compra->sustento_venta0 == '403'){
                                 $datos[1]['nc'] = floatval($datos[1]['nc']) + $detallenc->detalle_total; 
